fe6e97d540b1947fdf213b355673705dab1e0ec7
Ediz Yurdakul <edizyurdakul@gmail.com>
2026-08-15 21:10:12 +0300

    chore: implement initial hono route

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,24 +1,21 @@
-import type { ApiResponse } from "@mellith/types";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
+
+import apiary from "./routes/apiary";
 
 const clientUrl = process.env.CLIENT_URL ?? "http://localhost:5173";
 
 export const app = new Hono()
 
   .use(cors({ origin: clientUrl }))
-
-  .get("/", (c) => {
-    return c.text("Hello from the API!");
-  })
-
-  .get("/hello", async (c) => {
-    const data: ApiResponse = {
-      message: "Hello from the API!",
-      success: true,
-    };
-
-    return c.json(data, { status: 200 });
+  .route("/apiary", apiary)
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return err.getResponse();
+    }
+    console.error(err);
+    return c.text("Internal Server Error", 500);
   });
 
 export default {
---
+++ apps/server/src/routes/apiary.ts
@@ -0,0 +1,61 @@
+import { zValidator } from "@hono/zod-validator";
+import { insertApiarySchema, apiaries, db, updateApiarySchema } from "@mellith/db";
+import { eq } from "drizzle-orm";
+import { Hono } from "hono";
+import z from "zod";
+
+const app = new Hono();
+
+const idParamSchema = z.object({
+  id: z.uuid(),
+});
+
+app.get("/", async (c) => {
+  // TODO:  add limits and offsets from params, or default to
+  const limit = Number(c.req.query("limit") ?? 50);
+  const offset = Number(c.req.query("offset") ?? 0);
+  const rows = await db.select().from(apiaries).limit(limit).offset(offset);
+  return c.json(rows);
+});
+
+app.get("/:id", zValidator("param", idParamSchema), async (c) => {
+  const { id } = c.req.valid("param");
+  const [apiary] = await db.select().from(apiaries).where(eq(apiaries.id, id));
+  if (!apiary) return c.json({ error: "Not found" }, { status: 404 });
+  return c.json(apiary, { status: 200 });
+});
+
+app.post("/", zValidator("json", insertApiarySchema), async (c) => {
+  const body = c.req.valid("json");
+  const [apiary] = await db.insert(apiaries).values(body).returning();
+  return c.json(apiary, { status: 201 });
+});
+
+app
+  .patch(
+    "/:id",
+    zValidator("param", idParamSchema),
+    zValidator("json", updateApiarySchema),
+    async (c) => {
+      const { id } = c.req.valid("param");
+      const body = c.req.valid("json");
+
+      if (Object.keys(body).length === 0) {
+        return c.json({ error: "No fields to update" }, { status: 400 });
+      }
+
+      const [apiary] = await db.update(apiaries).set(body).where(eq(apiaries.id, id)).returning();
+
+      if (!apiary) return c.json({ error: "Not found" }, { status: 404 });
+      return c.json(apiary);
+    },
+  )
+  .delete("/:id", zValidator("param", idParamSchema), async (c) => {
+    const { id } = c.req.valid("param");
+    const [apiary] = await db.delete(apiaries).where(eq(apiaries.id, id)).returning();
+
+    if (!apiary) return c.json({ error: "Not found" }, { status: 404 });
+    return c.json(apiary);
+  });
+
+export default app;
